5a69e28de3faecdd13a7f057485050463ce5a6df
Grayson Allen <grayallen1991@gmail.com>
2022-06-14 17:47:00 -0400

    backEnd2Lab

---
+++ server/controller.js
@@ -0,0 +1,44 @@
+const houses = require('./db.json')
+
+let globalID = 4
+
+module.exports = {
+    getHouses: (req, res) => { 
+        res.status(200).send(houses) 
+    },
+    deleteHouse: (req, res) => {
+        let index = houses.findIndex(elem => elem.id === +req.params.id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
+    },
+    createHouse: (req, res) => {
+        let {address, price, imageURL} = req.body
+        let newHouse = {
+            address,
+            price,
+            imageURL,
+            id: globalID
+        }
+        houses.push(newHouse)
+        globalID++
+        res.status(200).send(houses)
+    },
+    updateHouse: (req, res) => {
+        let {id} = req.params
+        let {type} = req.body
+        let index = houses.findIndex(elem => +elem.id === +id)
+
+        if(houses[index].price <= 10000 && type === 'minus') {
+            houses[index].price = 0
+            res.status(200).send(houses)
+        }else if(type === 'plus') {
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        } else if(type === 'minus') {
+            houses[index].price -= 10000
+            res.status(200).send(houses)
+        } else {
+            res.status(400).send('Price not valid')
+        }
+    }
+}
